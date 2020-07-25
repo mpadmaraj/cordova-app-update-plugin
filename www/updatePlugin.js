9f6b94fddbebdbcc0843ea41f332e04134c8ca36
raj <padmaraj.m@gmail.com>
2020-07-25 21:30:16 +0530

    Update updatePlugin.js

--- www/updatePlugin.js
+++ www/updatePlugin.js
@@ -1,21 +1,17 @@
-// Empty constructor
-var exec = require('cordova/exec');
+var InAppUpdateLoader = function(require, exports, module) {
+  var exec = require('cordova/exec');
 
-function UpdatePlugin() {}
+  function UpdatePlugin() {}
 
-// The function that passes work along to native shells
-// Message is a string, duration may be 'long' or 'short'
-UpdatePlugin.prototype.update = function(successCallback, errorCallback, config) {
-  exec(successCallback, errorCallback, 'UpdatePlugin', 'update', [{ 'flexibleUpdateStalenessDays': config.flexibleUpdateStalenessDays, 'immediateUpdateStalenessDays': config.immediateUpdateStalenessDays }]);
-}
-
-// Installation constructor that binds updatePlugin to window
-UpdatePlugin.install = function() {
-  if (!window.plugins) {
-    window.plugins = {};
+  UpdatePlugin.prototype.update = function(successCallback, errorCallback, config) {
+    exec(successCallback, errorCallback, 'UpdatePlugin', 'update', [{ 'flexibleUpdateStalenessDays': config.flexibleUpdateStalenessDays, 'immediateUpdateStalenessDays': config.immediateUpdateStalenessDays }]);
   }
-  window.plugins.updatePlugin = new UpdatePlugin();
-  return window.plugins.updatePlugin;
+
+
+  var inAppUpdate = new UpdatePlugin();
+  module.exports = inAppUpdate;
 };
 
-cordova.addConstructor(UpdatePlugin.install);
+InAppUpdateLoader(require, exports, module);
+ 
+cordova.define("cordova/plugin/InAppUpdate", InAppUpdateLoader);
